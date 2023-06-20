d3ee48976e3734010073c5054f148e97bcd7f2fb
Igor Todorovski <itodorov@ca.ibm.com>
2023-06-20 16:38:21 -0400

    Initial commit

---
+++ cicd.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/c3270port.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/c3270port.git'), string(name: 'PORT_DESCRIPTION', value: '3270 termina' )]
+  }
+}
