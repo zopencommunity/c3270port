86461f6c96bf586a792c5438c08ae0c3b2949b26
Mike Fulton <mikefultonpersonal@gmail.com>
2024-06-26 19:42:57 -0400

    fix build problem for testcase

--- testing/dev.c
+++ testing/dev.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 #include <zos.h>
 
+/*
+ * Assertion failed: dev_null >= 0, file: ../../../../Common/Test/json_test.c, line: 124, function: main
+ * CEE5207E The signal SIGABRT was received.
+ */
 int main()
 {
   int fd = open("/dev/null", 0);
-  if (!fd) {
+  if (fd < 0) {
     perror("unable to open /dev/null");
     return 4;
   } else {
-    printf("Success.\n");
+    printf("Success. fd:%d\n", fd);
     return 0;
   }
 }
