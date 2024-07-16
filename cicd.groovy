983a3bfd170cac834c1acd8103944b00c90afe6b
Mike Fulton <mikefultonpersonal@gmail.com>
2024-07-15 22:28:55 -0400

    force to 3.1 to see if it fixes the IPV6 issue with testing
    Signed-off-by: Mike Fulton <mikefultonpersonal@gmail.com>

--- cicd.groovy
+++ cicd.groovy
@@ -9,6 +9,10 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/c3270port.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/c3270port.git'), string(name: 'PORT_DESCRIPTION', value: '3270 termina' )]
+    build job: 'Port-Pipeline', parameters: [
+      string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/c3270port.git'), 
+      string(name: 'PORT_DESCRIPTION', value: '3270 terminal' ), 
+      string(name: 'NODE_LABEL', value: "v3r1"
+    ]
   }
 }
